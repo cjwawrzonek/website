02a12fa02274bf1e93d1b921ff178d9912b0d5aa
Christian Wawrzonek <cjw@christians-MacBook-Pro.local>
2017-05-01 19:36:18 -0700

    Fixed some static file serving, still lots of errors. Added paths for all pages.

--- flaskapp.py
+++ flaskapp.py
@@ -2,15 +2,40 @@ from flask import Flask, render_template
 from flask import Response
 flask_app = Flask('flaskapp')
 
+###########################################
+# URL routes
+###########################################
+@flask_app.route('/columns.html')
+def columns_page():
+    return render_template('columns.html')
+
+
+@flask_app.route('/contact.html')
+def contact_page():
+    return render_template('contact.html')
+
+
 @flask_app.route('/')
-@flask_app.route('/index.html')
+@flask_app.route('/home.html')
 def home_page():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@flask_app.route('/about.html')
+def about_page():
+    return render_template('about.html')
+
+
+@flask_app.route('/resume.html')
+def resume_page():
+    return render_template('resume.html')
+
+
 
 @flask_app.route('/hello')
 def hello_world():
     return Response(
-        'Hello world from Flask!\n',
+        'Hello world!\n',
         mimetype='text/plain'
     )
 
