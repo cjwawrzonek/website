4319bfcc212d9202df17c6b392a676992e409cc8
System Administrator <root@christians-MacBook-Pro.local>
2017-05-07 22:30:44 -0700

    style changes, removed about page

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, redirect
 flask_app = Flask('flaskapp')
 
 ###########################################
 # URL routes
 ###########################################
+@flask_app.route('/')
+def root_page():
+	return redirect('/home.html')
+
 @flask_app.route('/columns.html')
 def columns_page():
     return render_template('columns.html')
@@ -14,16 +18,14 @@ def contact_page():
     return render_template('contact.html')
 
 
-@flask_app.route('/')
 @flask_app.route('/home.html')
 def home_page():
     return render_template('home.html')
 
-
-@flask_app.route('/about.html')
-def about_page():
-    return render_template('about.html')
-
+# Looks pretty bad. Consider adding later.
+# @flask_app.route('/about.html')
+# def about_page():
+#     return render_template('about.html')
 
 @flask_app.route('/resume.html')
 def resume_page():
@@ -36,5 +38,5 @@ def hello_world():
         'Hello world!\n',
         mimetype='text/plain'
     )
-    
+
 app = flask_app.wsgi_app
